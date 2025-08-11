0e39c671860599fe0d046a3a648fd1e0e1e24f00
Seiran <bo646ru@gmail.com>
2025-08-11 00:34:18 +0000

    quick fix :)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { ReviewModule } from './review/review.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, ReviewModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
