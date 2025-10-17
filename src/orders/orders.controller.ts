9b7a4cd5b983bad09e1cd7e0306f7e0ded48a4eb
Seiran <bo646ru@gmail.com>
2025-10-17 15:08:55 -0400

    Making all order requests Admin only

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,31 +10,37 @@ import {
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Admin } from 'src/is-admin/is-admin.decorator';
 
 @Controller('order')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Admin()
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
+  @Admin()
   @Get()
   findAll() {
     return this.ordersService.findAll();
   }
 
+  @Admin()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(id);
   }
 
+  @Admin()
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(id, updateOrderDto);
   }
 
+  @Admin()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ordersService.remove(id);
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Admin } from 'src/is-admin/is-admin.decorator';
 
 @Injectable()
 export class OrdersService {
